07b9a7b745cf992d4ddcf345d378faf85c58decd
swapnap <swapnap422@gmail.com>
2024-03-11 11:27:58 +0530

    commited

---
+++ shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+cd /usr/share/nginx/html
+unzip /tmp/web.zip
+cp /home/centos/roboshop.confg /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx  
+
